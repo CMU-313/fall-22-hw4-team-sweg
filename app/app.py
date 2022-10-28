88b4b8720d9207a65927a6ea4d0ddb997e52befe
Kyungmin Kim <kyminbb.kim@gmail.com>
2022-10-27 23:53:38 -0400

    Use `flask-restx` instead

--- app/app.py
+++ app/app.py
@@ -1,9 +1,8 @@
 from flask import Flask
-from app.handlers.routes import configure_routes
+from flask_restx import Api
 
 app = Flask(__name__)
-
-configure_routes(app)
-
+api = Api(app)
+    
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=80)
