b8c4d11505ef52ed37b0f2bfeb9d040940fa74fa
jihyochung <jihyo0609@gmail.com>
2022-11-01 17:01:53 -0400

    model list function created

--- app/services/model_service.py
+++ app/services/model_service.py
@@ -7,9 +7,14 @@ class ModelService:
 
     @staticmethod
     def get_model(model_id: int) -> Optional[ModelMetadata]:
-        # TODO (jihyo): Implement this function
         return ModelMetadata(model_class="logistic", learning_rate=0.1, k=2)
 
+    def get_model_list() -> list[ModelMetadata]:
+        # empty list so far, but should fetch the data from a
+        # specific directory later, and parse out the data into
+        # a list
+        return []
+        
     def train(model_metadata: ModelMetadata) -> TrainResult:
         # TODO (jaehoon) : Implement this function
         return TrainResult(model_id=1, train_acc=0.5, valid_acc=0.5)
